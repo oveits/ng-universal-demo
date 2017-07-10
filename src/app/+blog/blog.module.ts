976a4ae83367025e0f0e5186c33c959e83a1890a
vagrant <vagrant@ubuntu-trusty64-docker-2017-02>
2017-07-10 21:02:17 +0000

    cleaning

--- src/app/+blog/blog.module.ts
+++ src/app/+blog/blog.module.ts
@@ -8,14 +8,11 @@ import { Observable } from 'rxjs/Observable';
 @Component({
   selector: 'blog-view',
   templateUrl: './blog.module.html'
-  //template: `<h1 innerHtml="{{title}}"></h1><div innerHtml="{{content}}"></div>`
 })
 
 export class BlogView implements OnInit {
-  data: any = null;
   title: any = null;
   content: any = null;
-  public subs: Observable<string>;
 
   constructor(private http: Http) {
   }
@@ -28,10 +25,9 @@ export class BlogView implements OnInit {
     return this.http.get('https://public-api.wordpress.com/rest/v1.1/sites/oliverveits.wordpress.com/posts/3078')
                 .map((res: Response) => res.json())
                  .subscribe(data => {
-                        this.data = data;
-			this.title = this.data.title;
-			this.content = this.data.content;
-                        console.log(this.data);
+			this.title = data.title;
+			this.content = data.content;
+                        console.log(data);
                 });
   }
 
