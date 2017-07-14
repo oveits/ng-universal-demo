4fd420ade1f2456280817c35c0b6d3531eebc4c7
vagrant <vagrant@ubuntu-trusty64-docker-2017-02>
2017-07-14 22:27:31 +0000

    added toc and works, but error messages "document is not defined" and not visible toc in source view

--- src/app/+blog/blog.module.ts
+++ src/app/+blog/blog.module.ts
@@ -3,6 +3,7 @@ import {RouterModule} from '@angular/router'
 import { Http, Response, Headers } from '@angular/http';
 import 'rxjs/add/operator/map'
 import { Observable } from 'rxjs/Observable';
+import * as angular from "angular";
 
 
 @Component({
@@ -13,6 +14,7 @@ import { Observable } from 'rxjs/Observable';
 export class BlogView implements OnInit {
   title: any = null;
   content: any = null;
+  toc: any = null;
 
   constructor(private http: Http) {
   }
@@ -27,8 +29,56 @@ export class BlogView implements OnInit {
                  .subscribe(data => {
 			this.title = data.title;
 			this.content = data.content;
+                        this.toc = this.getToc(data.content);
                         console.log(data);
                 });
+  }
+
+  private getToc(content: any) {
+     var tmp = document.createElement("div");
+     //var tmp = angular.element(document).createElement("div");
+     //var tmp = angular.element('<div/>')
+     //tmp.appendChild(document.createTextNode("<h1>header 1</h1><h1>header 2</h1>"));
+     tmp.innerHTML = content;
+     console.log(tmp.innerHTML); // <p>Test</p>
+
+     //var h1array = tmp.getElementsByTagName("h1");
+     //var harray = tmp.querySelectorAll("h1, h2, h3, h4, h5, h6");
+     //for (var h of harray) {
+         //console.log(h.tagName + ": " + h.innerHTML);
+     //}
+
+     var myArrayOfNodes = [].slice.call(tmp.querySelectorAll("h1, h2, h3, h4, h5, h6"));
+     var toc = document.createElement("ul");
+     var target = toc;
+     myArrayOfNodes.forEach( 
+       function(value, key, listObj) { 
+	   console.log(value.tagName + ": " + value.innerHTML);
+	   if ( "H1" == value.tagName ) { 
+	       target = toc;
+           }
+           if ( "H2" == value.tagName && target == toc ) {
+               target = target.appendChild(document.createElement("ul"));
+           }
+           if ( "H1" == value.tagName || "H2" == value.tagName ) {
+               var li = target.appendChild(document.createElement("li"));
+	       li.innerHTML = value.innerHTML;
+           }
+	   //console.log(toc.innerHTML); 
+       }
+//,
+	       //"myThisArg"
+
+     );
+     
+     console.log(toc.innerHTML);
+
+     //var contentDOM = document.createTextNode(content);
+     //console.log(contentDOM.innerHTML);
+
+     return(
+         toc.innerHTML
+     );
   }
 
 }
