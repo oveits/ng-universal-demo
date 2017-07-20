f33b2f64b189ff4c874e0606190578d066e1dbe5
vagrant <vagrant@ubuntu-trusty64-docker-2017-02>
2017-07-20 21:44:10 +0000

    added demo scrollTo link in blog module

--- src/app/+blog/blog.module.ts
+++ src/app/+blog/blog.module.ts
@@ -3,6 +3,8 @@ import {RouterModule} from '@angular/router'
 import { Http, Response, Headers } from '@angular/http';
 import 'rxjs/add/operator/map'
 import { Observable } from 'rxjs/Observable';
+import { Router, NavigationEnd } from '@angular/router';
+import { ScrollToModule } from '@nicky-lenaers/ngx-scroll-to';
 
 
 @Component({
@@ -15,7 +17,21 @@ export class BlogView implements OnInit {
   content: any = null;
   toc: any = null;
 
-  constructor(private http: Http) {
+  constructor(private http: Http, private router: Router) {
+    // from: https://stackoverflow.com/questions/36101756/angular2-routing-with-hashtag-to-page-anchor
+    router.events.subscribe(s => {
+      if (s instanceof NavigationEnd) {
+        const tree = router.parseUrl(router.url);
+        if (tree.fragment) {
+          const element = document.querySelector("#" + tree.fragment);
+          if (element) { element.scrollIntoView(element); }
+        }
+      }
+    });
+  }
+
+  goTo(location: string): void {
+    window.location.hash = location;
   }
 
   ngOnInit(){
@@ -28,8 +44,9 @@ export class BlogView implements OnInit {
                  .subscribe(data => {
 			this.title = data.title;
 			this.content = data.content;
-                        this.toc = this.getToc(data.content);
-                        console.log(data);
+                        this.toc = this.getToc(this.content);
+                        //console.log(data);
+                        console.log("content = " + this.content);
                 });
   }
 
@@ -46,6 +63,7 @@ export class BlogView implements OnInit {
 
      // initialize a pointer that points to toc root:
      var pointer = toc;
+var k = 0;
 
      // loop through the array of headlines
      myArrayOfHeadlineNodes.forEach( 
@@ -63,14 +81,24 @@ export class BlogView implements OnInit {
                pointer = pointer.appendChild(document.createElement("ul"));
            }
 
+           if ("" == value.id) {
+               value.id = "id" + Date.now() + ++k;
+           } 
+
 	   // for each headline, create a list item with the corresponding HTML content:
            var li = pointer.appendChild(document.createElement("li"));
-	   li.innerHTML = value.innerHTML;
+	   li.innerHTML = '<a ng-click="gotoElement(' + "'" + value.id + "'" + ')">' + value.innerHTML + '</a>';
+	   //li.innerHTML = '<a href="./blog#' + value.id + '">' + value.innerHTML + '</a>';
+	   //li.innerHTML = '<a ng-click="goTo(' + "'" + value.id + "'" + ')">' + value.innerHTML + '</a>';
        }
      );
      
      // debugging:
      console.log(toc.innerHTML);
+
+     // add the random ids to the content:
+     //content = contentdiv.innerHTML;
+     this.content = contentdiv.innerHTML;
 
      return(toc.innerHTML);
   }
@@ -80,6 +108,7 @@ export class BlogView implements OnInit {
 @NgModule({
   declarations: [BlogView],
   imports: [
+    ScrollToModule.forRoot(),
     RouterModule.forChild([
       { path: '', component: BlogView, pathMatch: 'full'}
     ])
